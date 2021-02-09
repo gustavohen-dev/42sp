63f337a35e589da11657cad8a2bfa52e2af0767d
Gustavo <gustavohendesign@gmail.com>
2021-02-09 07:15:25 -0300

    test

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -1,13 +1,13 @@
 #include "libft.h"
 
-void    ft_bzero(void *str, size_t n)
+void	ft_bzero(void *str, size_t n)
 {
-    unsigned int count;
+	unsigned int count;
 
-    count = 0;
-    while (count != n)
-    {
-        ((unsigned char *)str)[count] = '\0';
-        count++;
-    }
+	count = 0;
+	while (count != n)
+	{
+		((unsigned char *)str)[count] = '\0';
+		count++;
+	}
 }
