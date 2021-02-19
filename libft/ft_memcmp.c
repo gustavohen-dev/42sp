312bfd6eac61bdeac2fdffe6226b9601f7b7a845
Gustavo <gustavohendesign@gmail.com>
2021-02-19 04:12:24 -0300

    Update

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: ghenriqu <ghenriqu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 00:25:50 by ghenriqu          #+#    #+#             */
-/*   Updated: 2021/02/19 04:10:47 by ghenriqu         ###   ########.fr       */
+/*   Updated: 2021/02/19 04:12:19 by ghenriqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 int		ft_memcmp(const void *str1, const void *str2, size_t n)
 {
 	size_t count;
-	unsigned int value;
+	int value;
 
 	count = 0;
 	if (str1 == str2 || n == 0)
 	{
 		return (0);
 	}
-	while (count < n)
+	while (count < n && count >= 0)
 	{
 		if (((unsigned char *)str1)[count] != ((unsigned char *)str2)[count])
 		{
